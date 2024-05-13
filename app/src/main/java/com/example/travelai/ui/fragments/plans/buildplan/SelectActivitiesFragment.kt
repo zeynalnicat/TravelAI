071775f7a280bfec75ad637a3240956d721dd39a
Nicat Zeynalli <nicat21308@gmail.com>
2024-05-13 20:27:42 +0400

    layout adjust

--- app/src/main/java/com/example/travelai/ui/fragments/plans/buildplan/SelectActivitiesFragment.kt
+++ app/src/main/java/com/example/travelai/ui/fragments/plans/buildplan/SelectActivitiesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,22 +29,25 @@ class SelectActivitiesFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
 
     }
 
     private fun setAdapter() {
         val lists = listOf(
             MoreCity("Restaurants", R.drawable.food, true, isBuildPlan = true),
-            MoreCity("Luxury Hotels", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("Cultural Experiences", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("Shopping", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("Art Museums", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("River Cruises", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("Restaurants", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("Restaurants", R.drawable.food, true,isBuildPlan = true),
-            MoreCity("Restaurants", R.drawable.food, true,isBuildPlan = true),
+            MoreCity("Luxury Hotels", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("Cultural Experiences", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("Shopping", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("Art Museums", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("River Cruises", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("Restaurants", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("Restaurants", R.drawable.food, true, isBuildPlan = true),
+            MoreCity("Restaurants", R.drawable.food, true, isBuildPlan = true),
         )
 
         adapter =
@@ -54,12 +58,6 @@ class SelectActivitiesFragment : Fragment() {
         binding.recyclerView.adapter = adapter
 
 
-        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
-            override fun onChanged() {
-                val selectedSeats = adapter.getSelected()
-                binding.cardSubmit.visibility = if(selectedSeats.isNotEmpty()) View.VISIBLE else View.GONE
-            }
-        })
     }
 
     private fun setNavigation() {
